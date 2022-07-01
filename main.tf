9e9356b3f361ace0d4c14ac962440f75cb8245a7
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-01 14:10:58 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+data "azurerm_resource_group" "RG" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.name_vnet
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.name_subnet
+  resource_group_name  = azurerm_resource_group.RG.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name_vnet
+  address_prefixes     = ["10.0.2.0/24"]
+}
