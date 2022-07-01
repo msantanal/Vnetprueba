3a1de3c143ac4580d82c0bfa2b9f18f969e0164d
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-01 15:27:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
-
 data "azurerm_resource_group" "RG" {
   name = var.resource_group_name
 }
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "Vnet" {
   name                = var.name_vnet
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.RG.location
-  resource_group_name = azurerm_resource_group.RG.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "Subnet" {
   name                 = var.name_subnet
-  resource_group_name  = azurerm_resource_group.RG.name
-  virtual_network_name = azurerm_virtual_network.vnet.name_vnet
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.name_vnet
   address_prefixes     = ["10.0.2.0/24"]
 }
