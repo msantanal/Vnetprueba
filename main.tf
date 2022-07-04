36d914473ccc2140ced1222d38f03d6211aa2e54
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-04 16:19:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,5 @@ resource "azurerm_subnet" "Subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = ["10.0.2.0/24"]
-  depends_on                      = [resource.Vnet]
+  depends_on           = [resource.Vnet]
 }
